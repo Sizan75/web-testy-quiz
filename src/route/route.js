ce98423ec5ab7d5b604e21568fa7ad7c6892b010
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-11 03:56:13 +0600

    error page added

--- src/route/route.js
+++ src/route/route.js
@@ -1,4 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
+import Blog from "../components/Blog/Blog";
+import ErrorPage from "../components/ErroPage/ErrorPage";
 import Home from "../components/Home/Home";
 import Main from "../components/Main/Main";
 import Quiz from "../components/Quiz/Quiz";
@@ -8,6 +10,7 @@ export const router= createBrowserRouter([
     {
         path:'/',
         element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
         children:[
             {
                 path: '/',
@@ -16,9 +19,15 @@ export const router= createBrowserRouter([
 
             },
             {
-                path: 'quiz/:quizId',
+                path: 'home/:quizId',
                 loader: async (params) => fetch(`https://openapi.programming-hero.com/api/quiz/${params.quizId}`),
                 element:<Quiz></Quiz>
+
+            }
+            ,
+            {
+                path: '/blog',
+                element:<Blog></Blog>
 
             }
         ]
