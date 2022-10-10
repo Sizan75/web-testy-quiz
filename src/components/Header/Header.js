6bc237cd2c967010e4ad575e149ef9dbc955f569
Sizan75 <sakiuzzaman26@gmail.com>
2022-10-11 00:18:49 +0600

    Header style done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import logo from '../../images/logo1111.png'
+import './Header.css';
 
 const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <nav className='header'>
+            <div className='brand'>
+                <Link to='/'><img src={logo} alt="" /></Link>
+             <Link to='/' >Web Testy Quiz</Link>  
+             </div> 
+            <div>
+                <NavLink to="/shop" className={({ isActive }) =>
+              isActive ? 'activeClassName' : undefined
+            }>Shop</NavLink>
+                <NavLink to="/orders" className={({ isActive }) =>
+              isActive ? 'activeClassName' : undefined
+            }>Orders</NavLink>
+                <NavLink to="/inventory" className={({ isActive }) =>
+              isActive ? 'activeClassName' : undefined
+            }>Inventory</NavLink>
+                <NavLink to="/about" className={({ isActive }) =>
+              isActive ? 'activeClassName' : undefined
+            }>About</NavLink>
+            </div>
+        </nav>
     );
 };
 
